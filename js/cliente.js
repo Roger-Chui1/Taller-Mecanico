d5606273a7a0de3bf41051af478b37547f84f06b
Roger <65433947+Roger-Chui1@users.noreply.github.com>
2021-09-30 21:49:23 -0400

    Update cliente.js

--- js/cliente.js
+++ js/cliente.js
@@ -55,29 +55,6 @@ class DatosVehiculo {
     return this.placa;
   }
 }
-/*let fun = () => {
-  let d1 = document.getElementById("d1").value;
-  let d2 = document.getElementById("d2").value;
-  let d3 = document.getElementById("d3").value;
-  let d4 = document.getElementById("d4").value;
-  let d5 = document.getElementById("d5").value;
-  let d6 = document.getElementById("d6").value;
-  let d7 = document.getElementById("d7").value;
-  let res = new OrdenDeTrabajo(d1, d2, d3, d4, d5, d6, d7);
-  let datos = [res];
-  let dat = datos.length - 1;
-  let texto = ` <tr>
-                <td>${dat + 1}</td>
-                <td>${datos[dat].getNombre()}</td>
-                <td>${datos[dat].getDireccion()}</td>
-                <td>${datos[dat].getTelefono()}</td>
-                <td>${datos[dat].getMarca()}</td>
-                <td>${datos[dat].getModelo()}</td>
-                <td>${datos[dat].getColor()}</td>
-                <td>${datos[dat].getPlaca()}</td>
-            </tr>`;
-  document.getElementById("mostrar").innerHTML = texto;
-};*/
 $(document).ready(function () {
   let res1;
   let res2;
